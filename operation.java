c0d2bc4eb978fe8e4244e4fd8bc14fe7c2e79b1a
Reyanetse Sefehle <142493263+sefehle@users.noreply.github.com>
2025-04-19 17:58:18 +0200

    Create operation.java

---
+++ operation.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        double reactionResult =0;
+        double chemicalA= 10.2;
+        int chemicalB = 15;
+        reactionResult = ( chemicalA + chemicalB)/(chemicalA * chemicalB);
+        System.out.println(reactionResult);
+    }
+}
