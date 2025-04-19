82ce998db347218d9f07fad340f7218a026e046c
Reyanetse Sefehle <142493263+sefehle@users.noreply.github.com>
2025-04-20 00:39:23 +0200

    Create logicalOperators.java

---
+++ logicalOperators.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+     int weight = 40;
+     int height = 120;
+     if(weight >= 40 && height >= 120){
+         System.out.println("Congrats! You can ride!🎢");
+     }else {
+         System.out.println("Sorry, You can't ride today.😞");
+     }
+        
+    }
+}
