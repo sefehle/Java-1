374d56ec81a3037e53e05b5004afd46f23ff6ea6
Reyanetse Sefehle <142493263+sefehle@users.noreply.github.com>
2025-04-20 01:28:46 +0200

    Create whileLoop.java

---
+++ whileLoop.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+     int line = 1;
+     while (line <=15){
+         System.out.println("line" + line + ": LET'S GO <TEAM>!");
+         line = line + 1;
+     }  
+    }
+}
