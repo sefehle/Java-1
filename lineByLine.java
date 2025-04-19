4af4b5352ef19df092a57dd1aba610bd6b3be8f7
Reyanetse Sefehle <142493263+sefehle@users.noreply.github.com>
2025-04-19 17:56:50 +0200

    Create lineByLine.java

---
+++ lineByLine.java
@@ -0,0 +1,7 @@
+public class Helloworld{
+  public static void main (String[] args){
+   System.out.println("Today, on the menu is mac and cheese1");
+   System.out.println("Write a recipe that a user can follow to make today's special");
+   System.out.println("Mo wrong answers! Give the user you mac and cheese recipe!");
+   }
+}
