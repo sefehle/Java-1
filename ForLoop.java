de6394c4a08195c1115dd28ed29115965802cc7c
Reyanetse Sefehle <142493263+sefehle@users.noreply.github.com>
2025-04-20 01:29:15 +0200

    Create ForLoop.java

---
+++ ForLoop.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+     int num = 50;
+     for (int i = 0; i<50; i++){
+         System.out.println("I will be great at programming! 🚀");
+     }
+        
+    }
+}
